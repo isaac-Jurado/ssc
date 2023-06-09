fd44c2df9978df8680412b5d99862c278b66f087
isaac-Jurado <kuasi54@hotmail.com>
2023-06-08 18:47:44 -0600

    coneccion

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -3,7 +3,7 @@
         public function conectar(){
             $servidor= "localhost";
             $usuario= "root";
-            $password= "autodidacta";
+            $password= "";
             $db= "ssc";
             $conexion= mysqli_connect($servidor,$usuario,$password,$db);
             return $conexion;
