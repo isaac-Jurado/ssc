3afa41dcaf9066ea3e138084b1a1483e5f6bcccd
isaac-Jurado <kuasi54@hotmail.com>
2023-07-03 19:45:44 -0600

    graficasproblados

--- clases/informes.php
+++ clases/informes.php
@@ -2,17 +2,47 @@
 	include "Conexion.php";
 
     class graficas extends Conexion {
-        public function build_report($year){
+        public function reporte_tecomitl($year){
 	    	$total = array();
 	    	for($i=0; $i<12; $i++){
 	    		$month = $i+1;
-				$consulta = "SELECT COUNT(id_delito) AS total FROM t_reportes WHERE MONTH(fecha) = '$month' AND YEAR(fecha) = '$year' AND id_delito=7 LIMIT 1";
+				$consulta = "SELECT COUNT(id_delito) AS total FROM t_reportes WHERE MONTH(fecha) = '$month' AND YEAR(fecha) = '$year' AND id_delito=7 AND id_poblado = 2 LIMIT 1";
 	    		$sql = $this->conectar()->query($consulta);	
 	    		$total[$i] = 0;
 	    		foreach ($sql as $key){ $total[$i] = ($key['total'] == null)? 0 : $key['total']; }
 	    	}			 
 			return $total;
 	    }
+        public function reporte_milpa($year){
+	    	$total = array();
+	    	for($i=0; $i<12; $i++){
+	    		$month = $i+1;
+				$consulta = "SELECT COUNT(id_delito) AS total FROM t_reportes WHERE MONTH(fecha) = '$month' AND YEAR(fecha) = '$year' AND id_delito=7  AND id_poblado = 12 LIMIT 1";
+	    		$sql = $this->conectar()->query($consulta);	
+	    		$total[$i] = 0;
+	    		foreach ($sql as $key){ $total[$i] = ($key['total'] == null)? 0 : $key['total']; }
+	    	}			 
+			return $total;
+	    }
+        public function reporte_francisco($year){
+	    	$total = array();
+	    	for($i=0; $i<12; $i++){
+	    		$month = $i+1;
+				$consulta = "SELECT COUNT(id_delito) AS total FROM t_reportes WHERE MONTH(fecha) = '$month' AND YEAR(fecha) = '$year' AND id_delito=7 AND id_poblado = 4 LIMIT 1";
+	    		$sql = $this->conectar()->query($consulta);	
+	    		$total[$i] = 0;
+	    		foreach ($sql as $key){ $total[$i] = ($key['total'] == null)? 0 : $key['total']; }
+	    	}			 
+			return $total;
+	    }
+        public function report_buil($year){
+	    	$tecomitlsql = $this->reporte_tecomitl($year);
+            $milpasql = $this->reporte_milpa($year);
+            $franciscosql = $this->reporte_francisco($year);
+		    return ["tecomitlsql" => $tecomitlsql, "milpasql" => $milpasql,"franciscosql"=>$franciscosql];
+	    }			 
+			
+	    
     }
 
 
--- public/js/cliente/delitos.js
+++ public/js/cliente/delitos.js
@@ -1,5 +1,8 @@
+var today = new Date();
+var año = today.getFullYear();
 
-mostraResultado(2023);
+
+mostraResultado(año);
 
 function mostraResultado(year){
     $('.resultados').html('<canvas id="tecomitl"></canvas>');
@@ -11,7 +14,7 @@ function mostraResultado(year){
         success:function(response){
             console.log(year)
             console.log(response);
-            var Datos = {
+            var tecomitl = {
                     labels : ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
                     datasets : [
                         {
@@ -19,12 +22,42 @@ function mostraResultado(year){
                             strokeColor : 'rgba(57,194,112,0.7)', //COLOR DEL BORDE DE LAS BARRAS
                             highlightFill : 'rgba(73,206,180,0.6)', //COLOR "HOVER" DE LAS BARRAS
                             highlightStroke : 'rgba(66,196,157,0.7)', //COLOR "HOVER" DEL BORDE DE LAS BARRAS
-                            data : response
+                            data : response.tecomitlsql
                         }
                     ]
-                }
+            }
+            var milpaAlta = {
+                labels : ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                datasets : [
+                    {
+                        fillColor : 'rgba(91,228,146,0.6)', //COLOR DE LAS BARRAS
+                        strokeColor : 'rgba(57,194,112,0.7)', //COLOR DEL BORDE DE LAS BARRAS
+                        highlightFill : 'rgba(73,206,180,0.6)', //COLOR "HOVER" DE LAS BARRAS
+                        highlightStroke : 'rgba(66,196,157,0.7)', //COLOR "HOVER" DEL BORDE DE LAS BARRAS
+                        data : response.milpasql
+                    }
+                ] 
+            }
+            var SanFrancisco = {
+                labels : ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                datasets : [
+                    {
+                        fillColor : 'rgba(91,228,146,0.6)', //COLOR DE LAS BARRAS
+                        strokeColor : 'rgba(57,194,112,0.7)', //COLOR DEL BORDE DE LAS BARRAS
+                        highlightFill : 'rgba(73,206,180,0.6)', //COLOR "HOVER" DE LAS BARRAS
+                        highlightStroke : 'rgba(66,196,157,0.7)', //COLOR "HOVER" DEL BORDE DE LAS BARRAS
+                        data : response.franciscosql
+                    }
+                ] 
+            }
             var contexto = document.getElementById('tecomitl').getContext('2d');
-            window.Barra = new Chart(contexto).Bar(Datos, { responsive : true });
+            window.Barra = new Chart(contexto).Bar(tecomitl, { responsive : true });
+            Barra.clear();
+            var contexto = document.getElementById('milpa').getContext('2d');
+            window.Barra = new Chart(contexto).Line(milpaAlta, { responsive : true });
+            Barra.clear();
+            var contexto = document.getElementById('francisco').getContext('2d');
+            window.Barra = new Chart(contexto).Line(SanFrancisco, { responsive : true });
             Barra.clear();
         }
     });
